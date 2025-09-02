9863cf081073e6021e249d7d35acd50ce212df0d
bavucadoz <aryabayusamodra@gmail.com>
2025-09-02 09:59:37 +0700

    Sistem Informasi Perbankan

---
+++ Minggu02/Nasabah.java
@@ -0,0 +1,10 @@
+package Minggu02;
+
+public class Nasabah {
+    public String nama;
+    public int saldo;
+
+    public void tampilkanSaldo() {
+        System.out.println("Saldo Anda adalah " + saldo);
+    }
+}
---
+++ Minggu02/TestPerbankan.java
@@ -0,0 +1,12 @@
+package Minggu02;
+
+public class TestPerbankan {
+    public static void main(String[] args) {
+        Nasabah nsb = new Nasabah();
+
+        nsb.nama = "Bill Gates";
+        nsb.saldo = 1000;
+
+        nsb.tampilkanSaldo();
+    }
+}
