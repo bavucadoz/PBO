2aa467806cfd90d7cbbee753c6b61a14a46d4a29
bavucadoz <aryabayusamodra@gmail.com>
2025-09-12 22:32:00 +0700

    Membuat Sistem informasi koperasi simpan pinjam

---
+++ Minggu03/SistemInformasiKoperasi/Anggota.java
@@ -0,0 +1,47 @@
+package Minggu03.SistemInformasiKoperasi;
+
+public class Anggota {
+    private String noKTP;
+    private String nama;
+    private int limitPinjaman;
+    private int jumlahPinjaman;
+
+    public Anggota(String noKTP, String nama, int limitPinjaman) {
+        this.noKTP = noKTP;
+        this.nama = nama;
+        this.limitPinjaman = limitPinjaman;
+        this.jumlahPinjaman = 0; 
+    }
+
+    public String getNoKTP() {
+        return noKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getLimitPinjaman() {
+        return limitPinjaman;
+    }
+
+    public int getJumlahPinjaman() {
+        return jumlahPinjaman;
+    }
+
+    public void pinjam(int jumlah) {
+        if (jumlahPinjaman + jumlah > limitPinjaman) {
+            System.out.println("Maaf, jumlah pinjaman melebihi limit.");
+        } else {
+            jumlahPinjaman += jumlah;
+        }
+    }
+
+    public void angsur(int jumlah) {
+        if (jumlah > jumlahPinjaman) {
+            System.out.println("Maaf, jumlah angsuran melebihi jumlah pinjaman.");
+        } else {
+            jumlahPinjaman -= jumlah;
+        }
+    }
+}
