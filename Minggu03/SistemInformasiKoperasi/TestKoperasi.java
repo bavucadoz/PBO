8a7d97427fe63cea7314c642e57bffa6a287acc0
bavucadoz <aryabayusamodra@gmail.com>
2025-09-12 22:47:29 +0700

    Menambahkan scanner untuk input nilai pinjaman dan angsuran

--- Minggu03/SistemInformasiKoperasi/TestKoperasi.java
+++ Minggu03/SistemInformasiKoperasi/TestKoperasi.java
@@ -1,10 +1,24 @@
 package Minggu03.SistemInformasiKoperasi;
 
+import java.util.Scanner;
 public class TestKoperasi {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Anggota donny = new Anggota("111333444", "Donny", 5000000); 
         System.out.println("Nama Anggota: " + donny.getNama());
         System.out.println("Limit Pinjaman: " + donny.getLimitPinjaman());
+
+        // Input pinjaman
+        System.out.print("\nMasukkan jumlah pinjaman: ");
+        int pinjaman = input.nextInt();
+        donny.pinjam(pinjaman);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
+
+        // Input angsuran
+        System.out.print("\nMasukkan jumlah angsuran: ");
+        int angsuran = input.nextInt();
+        donny.angsur(angsuran);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
 
         System.out.println("\nMeminjam uang 10.000.000..."); 
         donny.pinjam(10000000);
