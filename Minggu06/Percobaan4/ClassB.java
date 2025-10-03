f2d44518a0dad330650d49337a66fd66a78b76c3
bavucadoz <aryabayusamodra@gmail.com>
2025-10-03 22:48:29 +0700

    Percobaan 4 :  Super Constructor

---
+++ Minggu06/Percobaan4/ClassB.java
@@ -0,0 +1,7 @@
+package Minggu06.Percobaan4;
+
+public class ClassB extends ClassA {
+    ClassB() {
+        System.out.println("Konstruktor B dijalankan");
+    }
+} 
---
+++ Minggu06/Percobaan4/ClassC.java
@@ -0,0 +1,8 @@
+package Minggu06.Percobaan4;
+
+public class ClassC extends ClassB {
+    ClassC () {
+        super();
+        System.out.println("Konstruktor C dijalankan");
+    }
+}
