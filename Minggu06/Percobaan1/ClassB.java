3075cd3eaacbc9180c95343ebe144131cbfc9183
bavucadoz <aryabayusamodra@gmail.com>
2025-10-03 21:13:55 +0700

    Percobaan 1 - Extends

---
+++ Minggu06/Percobaan1/ClassB.java
@@ -0,0 +1,12 @@
+package Minggu06.Percobaan1;
+
+public class ClassB extends ClassA{
+    public int z;
+
+    public void getNilaiZ() {
+        System.out.println("Nilai z:" + z);
+    }
+    public void getJumlah() {
+        System.out.println("Jumlah:" + (x+y+z));
+    }
+}
