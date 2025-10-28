5c26628a19ed3d92ff0fc08dfa373f0bf23fb2c5
bavucadoz <aryabayusamodra@gmail.com>
2025-10-28 08:29:16 +0700

    PBO

--- Minggu07/Utama.java
+++ Minggu07/Utama.java
@@ -1,5 +1,15 @@
 package Minggu07;
 
 public class Utama {
-    
+    public static void main(String[] args) {
+        System.out.println("Program Testing Class Manager & Staff");
+        Manager man[] = new Manager[2];
+        Staff staff1[] = new Staff[2];
+        Staff staff2[] = new Staff[3];
+
+        man[0]= new Manager();
+        man[0].setNama("Tedjo");
+        man[0].setNip("101");
+        
+    }
 }
