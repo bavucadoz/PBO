2c047ffa46f19599e4c405fdea84880f2452e909
bavucadoz <aryabayusamodra@gmail.com>
2025-10-30 21:59:42 +0700

    Jobsheet 10 - Abstract Class

---
+++ Minggu10/Ikan.java
@@ -0,0 +1,10 @@
+package Minggu10;
+
+public class Ikan extends Hewan {
+    @Override
+    public void bergerak() {
+        System.out.println("Berenang dengan SIRIP, \"Wush..wush..\"");
+    }
+}
+
+    
